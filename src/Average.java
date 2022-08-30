b10559511985810dff16bce1c1cb2e49c0407641
sammikim <yrriksh@gmail.com>
2022-08-30 20:34:57 +0900

    Average

---
+++ src/Average.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String args) {
+
+        Scanner sc = new Scanner(System.in);
+
+       int N =  sc.nextInt();
+       String Snum = sc.next();
+       char[] cNum = Snum.toCharArray();
+       int sum = 0;
+       for(int i = 0; i < cNum.length; i++) {
+           sum += cNum[i];
+       }
+       System.out.println(sum);
+    }
+}
