a5c11296c2bd77569c91779050aa23591683ad16
sammikim <yrriksh@gmail.com>
2022-08-30 20:05:57 +0900

    Initial commit

---
+++ src/Count.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       String sNum = sc.next();
+       char[] cNum = sNum.toCharArray();
+       int sum = 0;
+
+       for (int i = 0; i < cNum.length; i++) {
+           sum += cNum[i];
+       }
+       System.out.println(sum);
+    }
+}
